403d0865b81719c879e00e26ac28409e57e25b38
Reema1234ag <reema.agrawal_cs18@gla.ac.in>
2021-05-10 16:11:34 +0530

    new

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,25 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import React,{ Component } from 'react';
+import greet from'./components/greet.js';
+class App extends Component {
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hello world!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <greet/>
     </div>
   );
+}
 }
 
 export default App;
---
+++ hello-world/src/components/greet.js
@@ -0,0 +1,6 @@
+import  React from 'react'
+
+function greet(){
+    return<h1>Hello Reema</h1>
+}
+export default greet
