93f14e1daab769a68cd6c6438a7e4f066d23cfdd
Reema1234ag <reema.agrawal_cs18@gla.ac.in>
2021-05-10 16:27:51 +0530

    update

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,7 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
 import React,{ Component } from 'react';
-import greet from'./components/greet.js';
 function App() {
   return (
     <div className="App">
