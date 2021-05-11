e83c205162290ba046b1e8fafff1b139ca8168f0
Reema1234ag <reema.agrawal_cs18@gla.ac.in>
2021-05-11 20:25:28 +0530

    concept of state and set state

--- hello-world/src/components/Classcomponent.js
+++ hello-world/src/components/Classcomponent.js
@@ -24,13 +24,66 @@ export class Classcomponent extends React.Component{
         );
     }
 }
-/* GEETING DATA USING STATE (state actually use to change data if components wants to change their data they recived through props)*/
  export class Classcomponent1 extends React.Component{
      constructor(){
-         super()
+         super();
      }
     render(){
         return(<h2> This is our new class component</h2>
             );
+    }
+}
+
+/* GEETING DATA USING STATE (state actually use to change data if components wants to change their data they recived through props)*/
+export class Classcomponent2 extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            roll:1,
+            name:"Reema Agrawal",
+
+        }
+    }
+    updateMe=()=>{
+        this.setState({
+            roll:2,
+            name:"Shivani Agrawal",
+        });
+    }
+    render(){
+        return(
+            <div>
+            <h2>{this.state.roll}</h2>
+            <h2>{this.state.name}</h2>
+            <button onClick={this.updateMe}>Update Me</button>
+            </div>
+        );
+    }
+}
+
+/* GEETING DATA USING props and updating using setstate*/
+export class Classcomponent3 extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={                                             /*here props data will be copied in state and then set state will update that*/
+            roll:this.props.roll,
+            name:this.props.name,
+
+        }
+    }
+    updateMe=()=>{
+        this.setState({
+            roll:5,
+            name:"Reema Agrawal",
+        });
+    }
+    render(){
+        return(
+            <div>
+            <h2>{this.state.roll}</h2>
+            <h2>{this.state.name}</h2>
+            <button onClick={this.updateMe}>Update Me</button>
+            </div>
+        );
     }
 }
