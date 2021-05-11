8f767889ef4875219fdf6ac269fab6c21ee31108
Reema1234ag <reema.agrawal_cs18@gla.ac.in>
2021-05-11 17:02:06 +0530

    learning components

---
+++ hello-world/src/components/Classcomponent.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+export class Classcomponent extends React.Component{
+    render(){
+        return(<h2> This is our 1st class component</h2>
+            );
+    }
+}
+ export class Classcomponent1 extends React.Component{
+    render(){
+        return(<h2> This is our new class component</h2>
+            );
+    }
+}
---
+++ hello-world/src/components/FunctionalComponent.js
@@ -0,0 +1,7 @@
+import React from 'react'
+function FunctionalComponent(){
+    return(
+       <h2>This is our 1st FUNCTIONAL COMPONENT</h2> 
+    )
+}
+export default FunctionalComponent
