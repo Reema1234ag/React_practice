daf7c04458b2196f34ec8e889011532870eecc76
Reema1234ag <reema.agrawal_cs18@gla.ac.in>
2021-05-13 19:17:24 +0530

    list rendering

--- hello-world/src/youtube classes/List.js
+++ hello-world/src/youtube classes/List.js
@@ -34,7 +34,7 @@ export  function List1() {
             Course:"B.tech",
             Skill:"Android",
         },]
-        const personeList= persons.map(person =><Person person={person}/> )     //passing object(person) to another component//passing data as props
+        const personeList= persons.map(person =><Person person={person}/> )   //passing object(person) to another component//passing data as props
     return (
         <div>
           {personeList}
