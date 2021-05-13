e67a31a962b3a88b2f54609a2e6523d199dc77de
Reema1234ag <reema.agrawal_cs18@gla.ac.in>
2021-05-14 00:32:57 +0530

    react fragment

---
+++ hello-world/src/youtube classes2/Fragment.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Fragment() {
+    return (
+        <>
+         <h2>Hi this is React Fragment</h2>   
+         <p>It removes extra div from the DOM </p>
+        </>
+    )
+}
