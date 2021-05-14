fb1d4d2e83c02e0063fffc30f21bd274a364a753
Reema1234ag <reema.agrawal_cs18@gla.ac.in>
2021-05-15 02:06:04 +0530

    creating context

---
+++ hello-world/src/youtube classes4/Context/UserContext.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const UserContext=React.createContext('Reema Default')    //adding default value is provider is not there
+//every context object created with usercontext comes up with a provider and a consumer
+const UserProvider=UserContext.Provider
+const UserConsumer=UserContext.Consumer
+
+export {UserProvider,UserConsumer}
+export default UserContext   //for getting data using context-type property
