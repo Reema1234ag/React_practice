ef9b49523bdfb787dc7f028a83a1bf44ba9ad874
Reema1234ag <reema.agrawal_cs18@gla.ac.in>
2021-05-14 20:07:06 +0530

    catch error

--- hello-world/src/youtube classes3/Portals/PortalDemo.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-export default function PortalDemo() {
-    return ReactDom.createPortal(
-        <h2>Portal Demo </h2>,
-        document.getElementById('portal-root')
-        )
-}
