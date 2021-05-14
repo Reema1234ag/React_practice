b3e2853bdf54c73c19fe63bfa4c17fc8065d60e6
Reema1234ag <reema.agrawal_cs18@gla.ac.in>
2021-05-14 21:05:59 +0530

    portals

--- hello-world/src/youtube classes3/Portals and Error Boundary/PortalDemo.js
+++ hello-world/src/youtube classes3/Portals and Error Boundary/PortalDemo.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+const css={
+    backgroundColor:"yellow",
+}
 export default function PortalDemo() {
     return ReactDom.createPortal(
-        <h2>Portal Demo </h2>,
+        <h2>Portal Demo<span style={css}>(In 'portal-root DOM node)</span></h2>,
         document.getElementById('portal-root')
         )
 }
